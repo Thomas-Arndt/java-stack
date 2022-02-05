669f4dc0aa25db157551cb567c64edfae402e075
Thomas Arndt <arndtt42@gmail.com>
2022-02-04 16:02:58 -0800

    Assignment to practice writing, compiling, running a java program.

---
+++ foundations/first_java_program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name it Thomas Arndt.");
+        System.out.println("I am 33 years old.");
+        System.out.println("My hometown in Boulder, CO.");
+    }
+}
