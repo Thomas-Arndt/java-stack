cf72438690f96dfc09fe3c0a94ef2ed054da213b
Thomas Arndt <arndtt42@gmail.com>
2022-02-08 10:41:23 -0800

    Assignment to continue working with oop and java packages

---
+++ oop/zoo_keeper_part_2/src/zoo_keeper_part_2/Bat.java
@@ -0,0 +1,23 @@
+package zoo_keeper_part_2;
+
+public class Bat extends Mammal{
+	
+	public Bat() {
+		this.setEnergyLevel(300);
+	}
+	
+	public void fly() {
+		System.out.println("\nThe bat flies around (*fwip fwip fwip*)");
+		this.setEnergyLevel(getEnergyLevel()-50);
+	}
+	
+	public void eatHuman() {
+		System.out.println("\nThe bat just...ate...a...human...!!!");
+		this.setEnergyLevel(getEnergyLevel()+25);
+	}
+	
+	public void attackTown() {
+		System.out.println("\nThe bat set the town on fire!!!");
+		this.setEnergyLevel(getEnergyLevel()-100);
+	}
+}
---
+++ oop/zoo_keeper_part_2/src/zoo_keeper_part_2/TestZoo.java
@@ -0,0 +1,18 @@
+package zoo_keeper_part_2;
+
+public class TestZoo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bat bat1 = new Bat();
+		bat1.attackTown();
+		bat1.attackTown();
+		bat1.attackTown();
+		bat1.eatHuman();
+		bat1.eatHuman();
+		bat1.fly();
+		bat1.fly();
+		bat1.displayEnergy();
+	}
+
+}
