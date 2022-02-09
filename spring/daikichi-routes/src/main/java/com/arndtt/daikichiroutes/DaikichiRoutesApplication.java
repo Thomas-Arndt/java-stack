ab0259ff264749e889658006b83c934c8e190f3f
Thomas Arndt <arndtt42@gmail.com>
2022-02-08 16:42:47 -0800

    First assignment using Spring Boot

---
+++ spring/daikichi-routes/src/main/java/com/arndtt/daikichiroutes/DaikichiRoutesApplication.java
@@ -0,0 +1,32 @@
+package com.arndtt.daikichiroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiRoutesApplication.class, args);
+	}
+	
+	@RequestMapping("")
+	public String welcome() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow and opportunity will arise, so be sure to be open to new ideas!";
+	}
+
+}
