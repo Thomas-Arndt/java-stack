2b3303c0ae360217e7ee65c2065f2766a70b43ce
Thomas Arndt <arndtt42@gmail.com>
2022-02-16 11:11:50 -0800

    Changed required jre to 11

--- oop/zoo_keeper_part_2/src/zoo_keeper_part_2/TestZoo.java
+++ oop/zoo_keeper_part_2/src/zoo_keeper_part_2/TestZoo.java
@@ -3,7 +3,6 @@ package zoo_keeper_part_2;
 public class TestZoo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Bat bat1 = new Bat();
 		bat1.attackTown();
 		bat1.attackTown();
