8381f8ac08a93cdcb88b16d62bf55b42ffb10770
Thomas Arndt <arndtt42@gmail.com>
2022-02-17 11:34:46 -0800

    Added @Repository annotation

--- spring/lookify/src/main/java/com/arndtt/lookify/repositories/SongRepository.java
+++ spring/lookify/src/main/java/com/arndtt/lookify/repositories/SongRepository.java
@@ -3,9 +3,11 @@ package com.arndtt.lookify.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.arndtt.lookify.models.Song;
 
+@Repository
 public interface SongRepository extends CrudRepository<Song, Long> {
 	List<Song> findAll();
 	List<Song> findByArtistContaining(String artist);
